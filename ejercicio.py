ae23ef7e1c53f1b27c85cf47cabef5b5a5d04b9f
arotena <albertosss27@gmail.com>
2016-02-07 10:47:39 +0100

    He realizado el ejercicio

--- ejercicio.py
+++ ejercicio.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# hola, hola
-
-fd = open('/etc/passwd', 'r')
-
+fd = open("/etc/passwd","r");
 lineas = fd.readlines()
 fd.close()
-
+diccionario = {}
+keys = [ "root", "imaginario"]
 for linea in lineas:
-    elementos = linea.split(':')
-    print elementos[0], elementos[-1][:-1]
+    if not linea:
+        break
+    corte = linea.split(':')
+    login = corte[0]
+    shell = corte[-1]
+    diccionario[login] = shell[:-1]
+for key in keys:
+    encontrado = key in diccionario
+    if encontrado != 0:
+        print key + ":", diccionario[key]
+    else:
+        print key + ": No encontrado"
